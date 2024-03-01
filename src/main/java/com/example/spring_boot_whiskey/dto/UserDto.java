27bfd9f6517779ee90f5b8f4d12b80a5e791bd1e
hasimovagm <gunay.hasimova@kapitalbank.az>
2024-03-01 21:19:40 +0400

    add validation errorHandler

--- src/main/java/com/example/spring_boot_whiskey/dto/UserDto.java
+++ src/main/java/com/example/spring_boot_whiskey/dto/UserDto.java
@@ -1,13 +1,11 @@
 package com.example.spring_boot_whiskey.dto;
 
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import java.io.Serializable;
 import lombok.Data;
 
 @Data
-public class UserDto implements Serializable{
+public class UserDto{
 
     @NotBlank(message = "Name can not be null")
     private String name;
--- src/main/java/com/example/spring_boot_whiskey/exception/ErrorHandler.java
+++ src/main/java/com/example/spring_boot_whiskey/exception/ErrorHandler.java
@@ -1,32 +1,39 @@
 package com.example.spring_boot_whiskey.exception;
 
 import java.util.Arrays;
-import java.util.List;
-import jakarta.validation.ConstraintViolation;
-import jakarta.validation.ConstraintViolationException;
-import java.util.Set;
-import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @Slf4j
 @RestControllerAdvice
-public class ErrorHandler extends ResponseEntityExceptionHandler {
+public class ErrorHandler {
 
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    @ExceptionHandler(MyException.class)
-    public ErrorResponse handleInternalException(MyException e) {
-        var response = ErrorResponse.build(
-                HttpStatus.NOT_FOUND, e.getMessage());
-        log.error("Internal Server Error: {}", response);
-        if (log.isTraceEnabled()) {
-            log.error(Arrays.toString(e.getStackTrace()));
-        }
-        return response;
+  @ResponseStatus(HttpStatus.NOT_FOUND)
+  @ExceptionHandler(MyException.class)
+  public ErrorResponse handleInternalException(MyException e) {
+    var response = ErrorResponse.build(
+            HttpStatus.NOT_FOUND, e.getMessage());
+    log.error("Internal Server Error: {}", response);
+    if (log.isTraceEnabled()) {
+      log.error(Arrays.toString(e.getStackTrace()));
     }
+    return response;
+  }
+
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ErrorResponse handleInternalException(MethodArgumentNotValidException e) {
+    var response = ErrorResponse.build(
+            HttpStatus.BAD_REQUEST, e.getMessage());
+    log.error("Invalid request");
+    if (log.isTraceEnabled()) {
+      log.error(Arrays.toString(e.getStackTrace()));
+    }
+    return response;
+  }
 
 }
